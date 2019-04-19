db6e456774d2644806af32eef8e66c3bc31b7434
唐欢 <huantang0529@gmail.com>
2019-04-19 10:47:46 +0800

    1. 过滤.vs目录 2. 上传出版场景以及入口脚本

---
+++ GitStudy/Assets/Scripts/MainGame.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/// <summary>
+/// 游戏主入口
+/// </summary>
+public class MainGame : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("MainGame()");
+        DontDestroyOnLoad(this);
+    }
+}
